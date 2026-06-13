030f41104476462cae53393e6704c28f6afda295
Steven <stevenericborkowski@gmail.com>
2026-06-13 06:59:25 -0400

    Create React Tailwind portfolio v2

---
+++ site/v2-react-tailwind/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <main className="min-h-screen bg-black text-white">
+      <h1 className="text-5xl font-bold text-red-500">
+        Steven Borkowski
+      </h1>
+    </main>
+  )
+}
+
+export default App
