453385fdf70edb47e0621586fb3fbcb06ea11198
Steven <stevenericborkowski@gmail.com>
2026-06-23 12:30:04 -0400

    fixed className in notfound

--- site/v2-react-tailwind/src/pages/NotFoundPage.jsx
+++ site/v2-react-tailwind/src/pages/NotFoundPage.jsx
@@ -11,12 +11,12 @@ function NotFoundPage() {
       <Header />
 
       <main id="main" className="page__main">
-        <section class="section section-hero">
-        <div class="container">
-          <div class="hero hero--404">
-            <p class="hero__eyebrow">oops...</p>
-            <h1 class="hero__title">404 - Page Not Found</h1>
-            <p class="hero__text">The page you’re looking for doesn’t exist or may have moved.</p>
+        <section className="section section-hero">
+        <div className="container">
+          <div className="hero hero--404">
+            <p className="hero__eyebrow">oops...</p>
+            <h1 className="hero__title">404 - Page Not Found</h1>
+            <p className="hero__text">The page you’re looking for doesn’t exist or may have moved.</p>
           </div>
         </div>
       </section>
